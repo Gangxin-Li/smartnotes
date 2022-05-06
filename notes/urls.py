0ba785a0a80e73009f7aab2c36f2dce40eee9c2a
Gangxin-Li <105022948+Gangxin-Li@users.noreply.github.com>
2022-05-06 01:15:15 +0100

    Building Dynamic Webpages
    Useful class to build webpages

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('notes',views.list),
-    path('notes/<int:pk>',views.detail),
+    path('notes',views.NodeListView.as_view()),
+    path('notes/<int:pk>',views.NodeDetailView.as_view()),
     ]
--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from .models import Notes
 from django.http import Http404
+from django.views.generic import ListView,DetailView
 # Create your views here.
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes_list.html',{'notes':all_notes})
+class NodeListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes_list.html"
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("note does not exist!")
-    return render(request,'notes_detail.html',{'note':note})
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes_list.html',{'notes':all_notes})
+
+class NodeDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes_detail.html"
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("note does not exist!")
+#     return render(request,'notes_detail.html',{'note':note})
