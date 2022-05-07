9d0dc6c1fde7002bebde1655950f1b3ae4f83c1c
Gangxin-Li <105022948+Gangxin-Li@users.noreply.github.com>
2022-05-07 17:01:57 +0100

    Static files in Django*
    Failed to load static css

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -120,9 +120,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_DIRS =[
+    BASE_DIR / 'Documents/smartnotes/static',
+    BASE_DIR / 'Documents/smartnotes/static/css',
+    '/Users/gangxinli/Documents/smartnotes/static/css',
+    '/Users/gangxinli/Documents/smartnotes/static'
+    
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
